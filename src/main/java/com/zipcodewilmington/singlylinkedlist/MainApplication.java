92038c6cf5fb80f9beb7d831f34294958fab5ee9
Christine <christine@zipcodersMBP13.localdomain>
2023-07-27 13:39:09 -0400

    wrote some code - gonna write tests next

--- src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
@@ -1,7 +1,11 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import com.zipcodewilmington.singlylinkedlist.Node;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class MainApplication {
+    public static void main(String[] args) {
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/NodeTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/NodeTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class NodeTest {
+public class NodeTest{
 
     @Test
     public void constructorTest(){
